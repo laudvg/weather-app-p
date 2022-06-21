d090e9fff49fbd54378429e47ac53393727ea167
Laura <laura.vivas.gamboa@gmail.com>
2022-06-21 23:56:58 +0200

    This adds the search bar to search for a city

---
+++ src/components/InputSearchWeather.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="input-group mb-3 w-5">
+    <input type="text" class="form-control" placeholder="Search a city" v-model="city" @keyup.enter="getCity(city)">
+    <button type="button" class="btn btn-light" @click="getCity(city)">Search</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+ data() {
+  return {
+    city:'',
+  }
+ },
+  props: {
+    getCity: Function,
+  },
+  methods: {
+
+  },
+
+});
+</script>
+
+<style>
+
+</style>
